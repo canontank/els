30a70ce8b1dcaff5ba820212d376c9e8d06570c5
canontank <58597814+canontank@users.noreply.github.com>
2020-03-24 23:40:36 +0900

    Delete example.js

--- js/example.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	var id = '1qyFWwTpoDyFf4grH3ipuSSk35fgxhSiPD-zq6aHih3E';
-	var worksheets = [''];
-	worksheets.forEach(function(worksheet) {
- 		$.googleSheetToJSON(id, worksheet).done(function(rows) {
-			console.log(rows);
-		});
-	});
-});
