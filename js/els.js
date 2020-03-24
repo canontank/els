bdca82bd78ab29e4427449080d3e1bbf9b25eb47
canontank <58597814+canontank@users.noreply.github.com>
2020-03-25 00:30:08 +0900

    Update els.js

--- js/els.js
+++ js/els.js
@@ -22,8 +22,8 @@ function setContents(rows) {
 	for (var row of rows) {
 		$("#els").append($('<tr/>')
 			.append($('<td/>', { 'align' : 'center' }).append($('<font/>', { text : row['stock'] } )))
-			.append($('<td/>', { 'align' :  'right' }).append($('<font/>', { text : row['index'], color : 'blue', bold : '600' } )))
-			.append($('<td/>', { 'align' :  'right' }).append($('<font/>', { text : row['rate'] } )))
+			.append($('<td/>', { 'align' :  'right' }).append($('<font/>', { text : row['index'] } )))
+			.append($('<td/>', { 'align' :  'right' }).append($('<font/>', { text : row['rate'], color : 'blue' } )))
 			.append($('<td/>', { 'align' :  'right' }).append($('<font/>', { text : row['crate'] } )))
 		);
 	}
