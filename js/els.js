6d03a0a6435cc72ab4c80ae2fb3a3e1d028ca034
canontank <58597814+canontank@users.noreply.github.com>
2020-03-24 23:47:51 +0900

    Update els.js

--- js/els.js
+++ js/els.js
@@ -1,11 +1,18 @@
 $(document).ready(function() {
-	var id = '1qyFWwTpoDyFf4grH3ipuSSk35fgxhSiPD-zq6aHih3E';
-	var worksheets = [''];
+	$("#els").append$('<tr/>')
+		.append($('<th/>', {}).append($('<font/>', { text : '종목' } )))
+		.append($('<th/>', {}).append($('<font/>', { text : '지수' } )))
+		.append($('<th/>', {}).append($('<font/>', { text : '비율' } )))
+	);
 	worksheets.forEach(function(worksheet) {
- 		$.googleSheetToJSON(id, worksheet)
+ 		$.googleSheetToJSON('1qyFWwTpoDyFf4grH3ipuSSk35fgxhSiPD-zq6aHih3E', [''])
 			.done(function(rows) {
 				console.log(rows);
-				console.log(rows("stock"));
+				$("#els").append($('<tr/>')
+					.append($('<td/>', { 'align' : 'center' }).append($('<font/>', { text : rows.get("stock") } )))
+					.append($('<td/>', { 'align' : ' right' }).append($('<font/>', { text : rows.get("index") } )))
+					.append($('<td/>', { 'align' :  'right' }).append($('<font/>', { text : rows.get("rate") } )))
+				);
 			}
 		);
 	});
