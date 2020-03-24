47f5f8b8de7b0c5db3dad3a419ced6767c8bbf43
canontank <58597814+canontank@users.noreply.github.com>
2020-03-25 00:07:13 +0900

    Update els.js

--- js/els.js
+++ js/els.js
@@ -1,21 +1,28 @@
 $(document).ready(function() {
+	var id = '1qyFWwTpoDyFf4grH3ipuSSk35fgxhSiPD-zq6aHih3E';
+	var worksheets = [''];
+	worksheets.forEach(function(worksheet) {
+ 		$.googleSheetToJSON(id, worksheet).done(function(rows) {
+			setTitle();		
+			setContents(rows);
+		});
+	});
+});
+
+function setTitle() {
 	$("#els").append($('<tr/>')
 		.append($('<th/>', {}).append($('<font/>', { text : '종목' } )))
 		.append($('<th/>', {}).append($('<font/>', { text : '지수' } )))
 		.append($('<th/>', {}).append($('<font/>', { text : '비율' } )))
 	);
-	var id = '1qyFWwTpoDyFf4grH3ipuSSk35fgxhSiPD-zq6aHih3E';
-	var worksheets = [''];
-	worksheets.forEach(function(worksheet) {
- 		$.googleSheetToJSON(id, worksheet).done(function(rows) {
-			for (var i = 0; i < rows.length; i++) {
-				var row = rows[i];
-				$("#els").append($('<tr/>')
-					.append($('<td/>', { 'align' : 'center' }).append($('<font/>', { text : row['stock'] } )))
-					.append($('<td/>', { 'align' :  'right' }).append($('<font/>', { text : row['index'] } )))
-					.append($('<td/>', { 'align' :  'right' }).append($('<font/>', { text : row['rate'] } )))
-				);
-			}
-		});
-	});
-});
+}
+
+function setContents(rows) {
+	for (var row of rows) {
+		$("#els").append($('<tr/>')
+			.append($('<td/>', { 'align' : 'center' }).append($('<font/>', { text : row['stock'] } )))
+			.append($('<td/>', { 'align' :  'right' }).append($('<font/>', { text : row['index'] } )))
+			.append($('<td/>', { 'align' :  'right' }).append($('<font/>', { text : row['rate'] } )))
+		);
+	}
+}
