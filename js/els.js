96eab73736dbb905df07eb7826f4f392994f76f5
canontank <58597814+canontank@users.noreply.github.com>
2020-03-24 23:17:28 +0900

    Add files via upload

---
+++ js/els.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	var id = '1qyFWwTpoDyFf4grH3ipuSSk35fgxhSiPD-zq6aHih3E';
+	var worksheets = [''];
+	worksheets.forEach(function(worksheet) {
+ 		$.googleSheetToJSON(id, worksheet)
+			.done(function(rows) {
+				console.log(rows);
+			}
+		);
+	});
+});
