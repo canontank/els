8d45f7253e7cb0529531f2f54dfa4ea9636019aa
canontank <58597814+canontank@users.noreply.github.com>
2020-03-24 23:11:11 +0900

    Update example.js

--- js/example.js
+++ js/example.js
@@ -2,10 +2,8 @@ $(document).ready(function() {
 	var id = '1qyFWwTpoDyFf4grH3ipuSSk35fgxhSiPD-zq6aHih3E';
 	var worksheets = [''];
 	worksheets.forEach(function(worksheet) {
- 		$.googleSheetToJSON(id, worksheet)
-			.done(function(rows) {
-				console.log(rows);
-			}
-		);
+ 		$.googleSheetToJSON(id, worksheet).done(function(rows) {
+			console.log(rows);
+		});
 	});
 });
