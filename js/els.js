3f262a94f41c6d3b2e10b7e0041db4fb28db2b54
canontank <58597814+canontank@users.noreply.github.com>
2020-03-25 11:38:46 +0900

    Add files via upload

--- js/els.js
+++ js/els.js
@@ -1,40 +1,80 @@
+var key = '1qyFWwTpoDyFf4grH3ipuSSk35fgxhSiPD-zq6aHih3E';
+var notiArr = new Array(
+	new Array( "2020-04-14", "85%" ),
+	new Array( "2020-10-14", "75%" ),
+	new Array( "2021-04-14", "85%" ),
+	new Array( "2021-10-14", "85%" ),
+	new Array( "2022-04-14", "80%" ),
+	new Array( "2022-10-14", "65%" )
+);
+
 $(document).ready(function() {
-	var id = '1qyFWwTpoDyFf4grH3ipuSSk35fgxhSiPD-zq6aHih3E';
 	var worksheets = [''];
 	worksheets.forEach(function(worksheet) {
- 		$.googleSheetToJSON(id, worksheet).done(function(rows) {
-			setTitle();		
-			setContents(rows);
+		$.googleSheetToJSON(key, worksheet).done(function(rows) {
+			setElsTitle();
+			setElsContents(rows);
+			setNotiTitle();
+			setNotiContents();
 		});
 	});
 });
 
-function setTitle() {
+function setElsTitle() {
 	$("#els").append($('<tr/>')
-		.append($('<th/>', {}).append($('<font/>', { text : '종목' } )))
-		.append($('<th/>', {}).append($('<font/>', { text : '지수' } )))
-		.append($('<th/>', {}).append($('<font/>', { text : '비율' } )))
-		.append($('<th/>', {}).append($('<font/>', { text : '변동' } )))
+		.append($('<th/>', { html : '종목' }))
+		.append($('<th/>', { html : '지수' }))
+		.append($('<th/>', { html : '변동' }))
+		.append($('<th/>', { html : '비율' }))
 	);
 }
 
-function setContents(rows) {
+function setElsContents(rows) {
 	for (var row of rows) {
 		$("#els").append($('<tr/>')
-			.append($('<td/>', { 'align' : 'center' }).append($('<font/>', { text : row['stock'] } )))
-			.append($('<td/>', { 'align' :  'right', style : 'font-weight : bold' }).append($('<font/>', { text : row['index'] } )))
-			.append($('<td/>', { 'align' :  'right', style : 'font-weight : bold' }).append($('<font/>', { text : row['rate'], color : getFontColor(row['index'], row['yindex']) } )))
-			.append($('<td/>', { 'align' :  'right' }).append($('<font/>', { text : row['crate'], color : getFontColor(row['index'], row['yindex']) } )))
+			.append($('<td/>', { html : row['stock'], class : 'stock' }))
+			.append($('<td/>', { html : row['index'], class : 'index' }))
+			.append($('<td/>', { html : row['rate'], class : getAddClass('rate', row['index'], row['yindex']) }))
+			.append($('<td/>', { html : getCRate(row['index'], row['yindex'], row['crate']), class : getAddClass('crate', row['index'], row['yindex']) }))
 		);
 	}
 }
 
-function getFontColor(index, yindex) {
+function getAddClass(oClass, index, yindex) {
 	if (index > yindex) {
-		return '#F93345';
+		oClass += ' red';
 	} else if (index < yindex) {
-		return '#003ACE';
+		oClass += ' blue';
+	}
+	return oClass;
+}
+
+function getCRate(index, yindex, crate) {
+	if (index > yindex) {
+		return '▲' + crate;
+	} else if (index < yindex) {
+		crate = crate.replace('-', '');
+		return '▼' + crate;
 	} else {
-		return '#333333';
+		return crate;
+	}
+}
+
+function setNotiTitle() {
+	$("#noti").append($('<tr/>')
+		.append($('<th/>', { html : '구분' }))
+		.append($('<th/>', { html : '날짜' }))
+		.append($('<th/>', { html : '배리어' }))
+	);
+}
+
+function setNotiContents() {
+	for (var i = 0; i < notiArr.length; i++) {
+		var noti = notiArr[i];
+		$("#noti").append($('<tr/>')
+			.append($('<td/>', { html : (i + 1) + "차 평가" }))
+			.append($('<td/>', { html : noti[0] }))
+			.append($('<td/>', { html : noti[1] }))
+		);
 	}
 }
